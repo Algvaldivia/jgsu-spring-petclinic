950cae0e0657b4f9fe2c82ec48ab0be7bd15ad26
Alexander Lázaro Gomez Valdivia <algvaldivia@gmail.com>
2023-02-23 17:15:26 +0000

    New Pipeline in Git repo

---
+++ src/pipeline.groovy
@@ -0,0 +1,30 @@
+pipeline {
+    agent any
+
+    stages {
+        stage('Checkout') {
+          steps {
+                // Get some code from a GitHub repository
+            git branch: 'main', url: 'https://github.com/Algvaldivia/jgsu-spring-petclinic.git'
+          }
+        }
+        stage('Build') {
+            steps {
+                // Run Maven on a Unix agent.
+                sh "./mvnw -Dmaven.test.failure.ignore=true clean package"
+
+                // To run Maven on a Windows agent, use
+                // bat "mvn -Dmaven.test.failure.ignore=true clean package"
+            }
+
+            post {
+                // If Maven was able to run the tests, even if some of the test
+                // failed, record the test results and archive the jar file.
+                always  {
+                    junit 'target/surefire-reports/*.xml'
+                    archiveArtifacts 'target/*.jar'
+                }
+            }
+        }
+    }
+}
